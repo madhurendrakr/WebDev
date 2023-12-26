15ae9071064e037673dafeacb2f8b7da5278dbf3
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2023-12-26 23:38:22 +0530

    third commit

--- js/loop.js
+++ js/loop.js
@@ -2,4 +2,6 @@ let num = 8;
 console.log("table of 8:");
 for (let i = 1; i <= 10; i++) {
   console.log(`${num} * ${i} = ${num * i}`);
-}
+
+
+ 
--- type2.js
+++ type2.js
@@ -1,3 +1,4 @@
-let name = "Madhurendra Kumar" ;
-console.log(name);
-console.log();
+let name1 = "Madhurendra Kumar";
+let name2 = "Varsha";
+console.log(name1);
+console.log(name2);
