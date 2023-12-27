cd6ee6840d1528969dcf118875f7f8961468d4c2
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2023-12-27 22:48:58 +0530

    fouth commit

---
+++ html/testa.js
@@ -0,0 +1,36 @@
+// [1:32 pm, 27/12/2023] Chanchal Bewara: // Your JavaScript function1
+
+function myFunction1() {
+  document.getElementById("demo").innerHTML = "Hello Raja";
+  document.getElementById("demo1").innerHTML = " Hi Chanchal";
+}
+myFunction1(); // Call your function
+
+//------------------------------------------------------------------------------
+// your javascript function_2
+
+function myFunction2() {
+  console.log("Hello JavaScript");
+}
+myFunction2();
+
+//--------------------------------------------------------------------------------
+
+function myFunction3(a, b) {
+  let c = a + b;
+  document.getElementById("sum").innerHTML = " Sum of two Numbers is " + c;
+}
+myFunction3(20, 30);
+
+// function 4 for printing something
+
+function myFunction4() {
+  let p = 3,
+    q = 19;
+  let r = p * q;
+
+  document.getElementById("pdt").innerHTML =
+    " product of two numbers is = " + r;
+}
+myFunction4();
+//------------------------------------------------------------------------------
