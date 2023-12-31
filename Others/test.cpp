e36bddd576a817185bf4138a33aa4fa9b2003cbc
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2023-12-31 23:32:21 +0530

    fifth

--- Others/test.cpp
+++ Others/test.cpp
@@ -1,5 +1,21 @@
-bool isPalindrome(const string& str) {
-    string reversedStr = str;
-    reverse(reversedStr.begin(), reversedStr.end());
-    return str == reversedStr;
-}
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+          unordered_map<string, vector<string>> mp;
+           vector<vector<string>> ans;
+        for (string s : strs) {
+            string t = s; 
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+       
+        for (auto p : mp) { 
+            ans.push_back(p.second);
+        }
+        return ans; 
+    }
+};
+
+And the some of the other of the sem and the some of the all of the sem and the some of the all of the transaction_safe_dynamic
+
+set safe_sql _update =1;
