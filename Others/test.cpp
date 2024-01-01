2fb066bbdf8871636c46e8a9505ae9d7e5359f15
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2024-01-01 22:59:46 +0530

    sixth

--- Others/test.cpp
+++ Others/test.cpp
@@ -1,21 +1,10 @@
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-          unordered_map<string, vector<string>> mp;
-           vector<vector<string>> ans;
-        for (string s : strs) {
-            string t = s; 
-            sort(t.begin(), t.end());
-            mp[t].push_back(s);
-        }
-       
-        for (auto p : mp) { 
-            ans.push_back(p.second);
-        }
-        return ans; 
-    }
-};
-
-And the some of the other of the sem and the some of the all of the sem and the some of the all of the transaction_safe_dynamic
-
-set safe_sql _update =1;
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 90;
+    int b = 89;
+    int c = a + b;
+    cout << "Sum is : " << c << endl;
+    return 0;
+}
