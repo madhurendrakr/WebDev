16b8f1916a1e36bd66a3f647cf3ead17ce992771
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2024-01-02 22:53:01 +0530

    first

---
+++ Others/test2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int num;
+    cout << "Enter num value: " << endl;
+    cin >> num;
+
+    while (num)
+    {
+        cout << num << endl;
+        num--;
+    }
+
+    return 0;
+}
