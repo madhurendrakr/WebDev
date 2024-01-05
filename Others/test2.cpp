510f34b524237bcf62b98d56d471ef8b2e1ba011
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2024-01-05 22:37:53 +0530

    none

--- Others/test2.cpp
+++ Others/test2.cpp
@@ -1,10 +1,11 @@
-
-
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int a, b;
+    cin >> a >> b;
     int c = a + b;
-    cout << c << endl;
+    cout << "sum is : " << c << endl;
+    return 0;
 }
